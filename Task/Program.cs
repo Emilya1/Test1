50882046c09671e0b8f41a3002ebc4240a0dac96
emilya <emilya.31.06@mail.ru>
2022-09-06 22:19:39 +0300

    Writing program

---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string[] ThreeCharacters(string[] array)
+{
+    string[] str = new string[array.Length];
+    for (int i = 0; i < str.Length; i++)
+{        if (array[i].Length <= 3)
+            str[i] = array[i];
+        
+}
+return str;
+}
+void ShowArray(string [] array)
+{
+ for(int i = 0; i < array.Length; i ++)
+ Console.Write(array[i] + " ");
+ Console.WriteLine();
+}
+string[] array = {"Россия","США","Сочи","11", "56" };
+string[] str = ThreeCharacters(array);
+ShowArray(array);
+ShowArray(str);
+
+
